2e3a1737dadd383b602a5afcc71aa65edbf95ac5
Jesper Andersson <Jesper_Forsa@hotmail.com>
2022-10-01 13:34:10 +0200

    ändring av hello world

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace edugit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Worldd!");
         }
     }
 }
